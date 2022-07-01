4c81763820ce28de7178deb12cd132451454adee
kovdmit <pzhabbiu@gmail.com>
2022-07-01 12:09:34 +0300

    Return to working condition. Replacing functions with classes in recipe/views.py

--- recipe/urls.py
+++ recipe/urls.py
@@ -4,8 +4,8 @@ from .views import *
 urlpatterns = [
     path('', Index.as_view(), name='home'),
     path('recipe/<str:recipe_slug>/', recipe_view, name='recipe_view'),
-    path('tag/<str:tag_slug>/', Tag.as_view(), name='tag_view'),
-    path('category/<str:category_slug>/', Cat.as_view(), name='category_view'),
+    path('tag/<str:tag_slug>/', tag_view, name='tag_view'),
+    path('category/<str:category_slug>/', category_view, name='category_view'),
     path('about/', about, name='about'),
     path('add_recipe/', add_recipe, name='add_recipe'),
 ]
